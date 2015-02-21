57a95499276c04f2ddfd6f01b12e586a9c368315
Stefan Podkowinski <github@midnightdrift.com>
2015-02-21 19:28:22 +0100

    fixed highlighting of last entered sig

--- web/js/igbtoolbox/signatures/input-result-ui.js
+++ web/js/igbtoolbox/signatures/input-result-ui.js
@@ -17,22 +17,32 @@ define(
     return defineComponent([withLogging, withPilot], {
 
       attached: function() {
-        this.on(document, sigEvents.SYSTEMS_UPDATED, this._renderReact);
-        this.on(sigEvents.SIGNATURE_INPUT_PARSED, this._parsedSignatures);
+        this.on(document, sigEvents.SYSTEMS_UPDATED, this._updateSignatures);
+        this.on(document, sigEvents.SIGNATURE_INPUT_PARSED, this._parsedSignatures);
       },
 
-      _renderReact: function(e, d) {
+      _updateSignatures: function(e, d) {
         var systemId = this.getPilot().systemId;
         var system = _.find(d.systems, function(s) {
           return s.systemId == systemId;
         });
         if(system && system.signatures) {
-          React.renderComponent(inputResultReact({signatures: system.signatures, lastEnteredSignatures: this._lastParsedSignatures}), this.node);
+          this._lastSignatures = system.signatures;
+        } else {
+          this._lastSignatures = [];
+        }
+        this._renderReact();
+      },
+
+      _renderReact: function() {
+        if(this._lastSignatures) {
+          React.renderComponent(inputResultReact({signatures: this._lastSignatures, lastEnteredSignatures: this._lastParsedSignatures}), this.node);
         }
       },
 
-      _parsedSignatures: function(e) {
-        this._lastParsedSignatures = _.map(e.signatures, function(s) { return s.signature; });
+      _parsedSignatures: function(e, d) {
+        this._lastParsedSignatures = _.map(d.signatures, function(s) { return s.signature; });
+        this._renderReact();
       }
 
     });
